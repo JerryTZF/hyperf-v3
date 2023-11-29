a4872340d6e141ec8245c496a25b8ed0efeb84e5
田朝帆 <tzfforyou@163.com>
2023-11-29 12:36:50 +0800

    代码优化

--- app/Controller/Test/ImageController.php
+++ app/Controller/Test/ImageController.php
@@ -12,7 +12,9 @@ declare(strict_types=1);
 
 namespace App\Controller\Test;
 
+use App\Constants\ConstCode;
 use App\Controller\AbstractController;
+use App\Lib\File\FileSystem;
 use App\Lib\Image\Qrcode;
 use App\Request\ImageRequest;
 use Hyperf\HttpMessage\Stream\SwooleStream;
@@ -26,6 +28,22 @@ use Zxing\QrReader;
 #[Controller(prefix: 'image')]
 class ImageController extends AbstractController
 {
+    // 制作二维码且上传到OSS返回二维码地址
+    #[Scene(scene: 'qrcode')]
+    #[PostMapping(path: 'qrcode/upload')]
+    public function uploadQrcodeToOss(ImageRequest $request): array
+    {
+        $config = $this->buildQrcodeConfig($request);
+        $qrCodeString = (new Qrcode($config))->getStream($config['content']);
+        $config['logo_path'] !== '' && unlink($config['logo_path']); // 移除logo临时文件
+
+        $fileFactory = new FileSystem();
+        $path = '/img/' . uniqid() . '_qrcode.png';
+        $fileFactory->write($path, $qrCodeString);
+
+        return $this->result->setData(['url' => ConstCode::OSS_DOMAIN . $path])->getResult();
+    }
+
     // 识别二维码
     #[Scene(scene: 'decode')]
     #[PostMapping(path: 'qrcode/decode')]
@@ -43,50 +61,49 @@ class ImageController extends AbstractController
         return $this->result->setData(['text' => $text])->getResult();
     }
 
+    // 下载二维码
+    #[Scene(scene: 'qrcode')]
+    #[PostMapping(path: 'qrcode/download')]
+    public function downloadQrcode(ImageRequest $request): MessageInterface|ResponseInterface
+    {
+        $config = $this->buildQrcodeConfig($request);
+        $qrCodeString = (new Qrcode($config))->getStream($config['content']);
+        $config['logo_path'] !== '' && unlink($config['logo_path']); // 移除logo临时文件
+
+        $tmpFilename = uniqid() . '.png';
+        return $this->response->withHeader('content-description', 'File Transfer')
+            ->withHeader('content-type', 'image/png')
+            ->withHeader('content-disposition', "attachment; filename={$tmpFilename}")
+            ->withHeader('content-transfer-encoding', 'binary')
+            ->withBody(new SwooleStream($qrCodeString));
+    }
+
     // 展示二维码
     #[Scene(scene: 'qrcode')]
     #[PostMapping(path: 'qrcode/show')]
     public function qrcode(ImageRequest $request): MessageInterface|ResponseInterface
     {
-        // 参数获取和处理
+        $config = $this->buildQrcodeConfig($request);
+        $qrCodeString = (new Qrcode($config))->getStream($config['content']);
+        $config['logo_path'] !== '' && unlink($config['logo_path']); // 移除logo临时文件
+
+        return $this->response->withHeader('Content-Type', 'image/png')
+            ->withBody(new SwooleStream($qrCodeString));
+    }
+
+    // 构建二维码配置
+    private function buildQrcodeConfig(ImageRequest $request): array
+    {
         $logo = $request->file('logo');
-        // 对 logo 进行判断处理
         if ($logo !== null) {
             $logoPath = BASE_PATH . '/runtime/upload/' . $logo->getClientFilename();
             $logo->moveTo($logoPath);
         } else {
             $logoPath = '';
         }
-        $default = [
-            'size' => 300,
-            'margin' => 30,
-            'logo_size' => 50,
-            'label_text' => '',
-            'mime' => 'png',
-            'foreground_color' => [0, 0, 0],
-            'background_color' => [255, 255, 255],
-            'content' => '',
-            'is_download' => false,
-        ];
-        $config = $request->inputs(array_keys($default), $default);
+        $config = $request->all();
         $config['logo_path'] = $logoPath;
 
-        $qrcode = new Qrcode($config);
-        $qrCodeString = $qrcode->getStream($config['content']);
-
-        $logoPath !== '' && unlink($logoPath);
-
-        if ($config['is_download']) {
-            $tmpFilename = uniqid() . '.png';
-            $response = $this->response->withHeader('content-description', 'File Transfer')
-                ->withHeader('content-type', 'image/png')
-                ->withHeader('content-disposition', "attachment; filename={$tmpFilename}")
-                ->withHeader('content-transfer-encoding', 'binary')
-                ->withBody(new SwooleStream($qrCodeString));
-        } else {
-            $response = $this->response->withHeader('Content-Type', 'image/png')
-                ->withBody(new SwooleStream($qrCodeString));
-        }
-        return $response;
+        return $config;
     }
 }
